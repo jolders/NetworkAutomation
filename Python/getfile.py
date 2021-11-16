73ff6529ce2aca032cd2f9de8f925d8cfa7d681b
jolders <94399885+jolders@users.noreply.github.com>
2021-11-16 14:33:15 +0000

    Update getfile.py

--- Python/getfile.py
+++ Python/getfile.py
@@ -1,2 +1,32 @@
-#! /bin/python
-# Get information from a XML / YML / 
+#! /usr/bin/python3
+# Get information from a JSON / XML / YML 
+# read a file from different file types and output the result.
+
+import json
+import yaml
+import xmltodict
+
+json_path = "db.json"
+yml_path = "db.yml"
+xml_path = "db.xml"
+
+# json output
+print('---- json output ----')
+with open(json_path) as f:
+    import json
+    data = json.load(f) 
+    print(data)
+
+# yml
+# pip install pyyaml
+print('---- yml output ----')
+with open(yml_path) as f:
+    data = yaml.safe_load(f) 
+    print(data)
+
+# xml
+# pip install xmltodict
+print('---- xml output ----')
+with open(xml_path) as f:
+    data = xmltodict.parse(f.read())["root"]
+    print(data)
